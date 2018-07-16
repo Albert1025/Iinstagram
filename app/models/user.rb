b976375de3cca3af9195e1864403f0c66dd82455
Albert Tan <tch_albert@hotmail.com>
2018-07-17 01:59:48 +0800

    added User, sessions, and associations

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  has_many :pics
+
+  has_secure_password
+
+  validates :first_name,
+  					presence: true
+  validates :last_name,
+  					presence: true
+  validates :email,
+  					presence: true,
+  					uniqueness: true,
+  					format: {
+  						with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  					}
+
+  def to_s
+  	"#{first_name} #{last_name}"
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715140812) do
+ActiveRecord::Schema.define(version: 20180716174756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 20180715140812) do
   create_table "pics", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pics_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
